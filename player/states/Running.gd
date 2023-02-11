60e5b37617d653f6aa4a5850ef3ff061a1b201ae
Cofica17 <63316032+Cofica17@users.noreply.github.com>
2023-02-11 09:29:52 +0100

    added sword_attack_1 first iteration

--- player/states/Running.gd
+++ player/states/Running.gd
@@ -1,6 +1,6 @@
 extends PlayerState
 
-@export var move_speed: float = 8
+@export var move_speed: float = 3
 @export var sprint_speed: float = 14
 @export var turn_speed: float = 10
 @export var acceleration: float = 10
